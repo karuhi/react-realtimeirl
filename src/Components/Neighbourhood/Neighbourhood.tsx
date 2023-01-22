35fce25c12638f4d45dd17be5275c3edcb645a32
かる <karuhi.dev@gmail.com>
2023-01-22 19:22:07 +0900

    fix: 現在位置を常時表示するように

--- src/Components/Neighbourhood/Neighbourhood.tsx
+++ src/Components/Neighbourhood/Neighbourhood.tsx
@@ -1,82 +1,29 @@
-import { useContext, useEffect, useState } from 'react';
-import { stateContext } from 'Contexts/StateContext';
+import { useContext } from "react";
+import { stateContext } from "Contexts/StateContext";
 
-import { Transition } from 'react-transition-group';
-import { useSpring, animated } from 'react-spring';
+import { Transition } from "react-transition-group";
 
-import isEmpty from 'Functions/isEmpty';
+import isEmpty from "Functions/isEmpty";
 
-import './Neighbourhood.scss';
+import "./Neighbourhood.scss";
 
 const Neighbourhood = () => {
   const [state] = useContext(stateContext);
-
-  const [show, setShow] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(0);
-  const [pendingVisible, setPendingVisible] = useState(false);
-
-  const springConfig = { mass: 4, tension: 400, friction: 80, velocity: 10 };
-  const neighbourhoodProps = useSpring({
-    config: springConfig,
-    transform: show ? `translate(0px, 0px)` : `translate(0px, -100px)`,
-    from: {
-      transform: show ? 'translate(-100px, 0px)' : 'translate(0px, 0px)',
-    },
-  });
-
-  useEffect(() => {
-    if (timeLeft === 0) {
-      setTimeLeft(0);
-    }
-    if (!timeLeft) return;
-    const interval = setInterval(() => {
-      setTimeLeft((timeLeft) => timeLeft - 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [timeLeft]);
-
-  useEffect(() => {
-    if (!pendingVisible) {
-      if (!timeLeft && !pendingVisible) {
-        setPendingVisible(true);
-        setTimeLeft(20);
-      }
-    }
-  }, [pendingVisible, timeLeft]);
-
-  useEffect(() => {
-    if (pendingVisible && !timeLeft) {
-      setPendingVisible(true);
-      setShow(true);
-      setTimeLeft(40);
-    }
-  }, [pendingVisible, timeLeft]);
-
-  useEffect(() => {
-    if (pendingVisible && show && !timeLeft) {
-      setPendingVisible(false);
-      setShow(false);
-      setTimeLeft(0);
-    }
-  }, [pendingVisible, show, timeLeft]);
 
   return (
     <Transition
       timeout={1000}
       mountOnEnter
       unmountOnExit
-      in={show && !isEmpty(state.locationData)}
+      in={!isEmpty(state.locationData)}
     >
-      <animated.div
-        className="neighbourhood-container"
-        style={neighbourhoodProps}
-      >
+      <div className="neighbourhood-container">
         <div className="neighbourhood">
           <hr />
-          {state.neighbourhood || 'Locating...'}
+          {state.neighbourhood || "Locating..."}
           <hr />
         </div>
-      </animated.div>
+      </div>
     </Transition>
   );
 };
